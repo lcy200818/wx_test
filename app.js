4a1ba19bfc40371c55f21b49865e0036dd0f2ddc
lcy200818 <lcy200818@aliyun.com>
2018-06-30 21:13:36 +0800

    添加同步操作

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var AV = require('leanengine');
+var async = require('async');
 
 // 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
 require('./cloud');
@@ -36,12 +37,35 @@ app.get('/', function(req, res) {
   // res.render('index', { currentTime: new Date() });
   var getdata=getServerData();
   res.send(getdata);
+
+
+    var tasks = {
+      table_a: function (callback) {
+        getServerData(function (err, result) {
+          if (err) {
+            // 异常后调用callback并传入err
+            callback(err);
+          } else {
+            console.log("执行成功");
+            // 执行完成后也要调用callback，不需要参数
+            callback(result);
+          }
+        });
+      },
+    };
+    //
+    async.series(tasks, function (err, results) {
+      if (err) {
+        res.send({ "status": "fail" });
+      } else {
+        res.send(results);
+      }
+    });
+
 });
 
 function getServerData() {
 
-    console.log("getServerData");
-    var temp = moment().format('YYYYMMDDHHmmssSSS');
     request.post(
         'http://www.zzj-ppai.com:2000/zzj/real-time/data',
         { json: { "SN": new Buffer("2017000000000128").toString('base64'), "CMD": "GetData" } },
